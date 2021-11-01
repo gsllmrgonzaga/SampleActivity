b8983aca1cd1f2e944190b0185eb43661763eac7
unknown <gsllmrgonzaga>
2021-11-01 17:58:44 +0800

    First commit

---
+++ app/src/main/java/com/example/sampleactivity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sampleactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        val firstFragment = FirstFragment();
+        val secondFragment = SecondFragment();
+    }
+}
