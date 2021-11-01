538729fe0035923ed563c3c221d8eb6ab06018e6
unknown <gsllmrgonzaga>
2021-11-01 18:14:55 +0800

    Added develop branch

--- app/src/main/java/com/example/sampleactivity/MainActivity.java
+++ app/src/main/java/com/example/sampleactivity/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        val firstFragment = FirstFragment();
-        val secondFragment = SecondFragment();
+//        val firstFragment = FirstFragment();
+//        val secondFragment = SecondFragment();
     }
 }
